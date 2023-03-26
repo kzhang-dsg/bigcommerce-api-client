9c8fd8625044233bedfc49649146a528b599ec30
kzhang <kzhang@dunnsolutions.com>
2023-03-26 18:56:46 -0500

    Fix generated webhook_Full model

--- src/model/generated/webhooks.v3/models/webhook_Full.ts
+++ src/model/generated/webhooks.v3/models/webhook_Full.ts
@@ -4,5 +4,26 @@
 
 import type { webhook_Base } from './webhook_Base';
 
-export type webhook_Full = webhook_Base;
+export type webhook_Full = (webhook_Base & {
+    /**
+     * ID of the webhook.
+     */
+    id?: number;
+    /**
+     * Client ID, unique to the store or app.
+     */
+    client_id?: string;
+    /**
+     * Permanent ID of the BigCommerce store.
+     */
+    store_hash?: string;
+    /**
+     * Time when the webhook was created.
+     */
+    created_at?: number;
+    /**
+     * Time when the webhook was updated.
+     */
+    updated_at?: number;
+});
 
