709c085ae4c5d6bd74ac8d47e4b9011d6d7a0b19
kzhang <kzhang@dunnsolutions.com>
2023-04-04 10:07:29 -0500

    Type product_Put_Collection does not contain IDs to update

--- src/model/generated/catalog.v3/models/product_Put_Collection.ts
+++ src/model/generated/catalog.v3/models/product_Put_Collection.ts
@@ -7,4 +7,9 @@ import type { product_Base } from './product_Base';
 /**
  * The model for batch updating products.
  */
-export type product_Put_Collection = Array<product_Base>;
+export type product_Put_Collection = Array<({
+    /**
+     * Unique ID of the *Product*. Read-Only.
+     */
+    readonly id: number;
+} & product_Base)>;
