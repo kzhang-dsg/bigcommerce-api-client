133dfb7e9b06d33e6e58fff3398c12790009b7d7
Siarhei Dudko <slavianich@gmail.com>
2023-04-04 16:08:56 +0400

    A batch update of catalog products must contain identifiers

--- src/model/generated/catalog.v3/models/product_Put_Collection.ts
+++ src/model/generated/catalog.v3/models/product_Put_Collection.ts
@@ -3,8 +3,9 @@
 /* eslint-disable */
 
 import type { product_Base } from './product_Base';
+import type { product_Full } from './product_Full';
 
 /**
  * The model for batch updating products.
  */
-export type product_Put_Collection = Array<product_Base>;
+export type product_Put_Collection = Array<product_Base & { id: product_Full['id'] }>;
